07d0feab7f40b0cfae2e24a7bc20eb1261895b58
xubury <xubury97@gmail.com>
2021-10-30 18:45:26 +0800

    * update script

--- .local/scripts/screenshot.sh
+++ .local/scripts/screenshot.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
-[ ! -d $HOME/Pictures/Screenshots ] && mkdir $HOME/Pictures/Screenshots
-maim $([ -n "$1" ] && echo "-us" || echo "-u") "$HOME/Pictures/Screenshots/$(date +%c).png" &> /dev/null
+[ ! -d $HOME/Pictures ] && mkdir $HOME/Pictures
+
+IMG_NAME="$HOME/Pictures/Screenshot.png"
+
+maim $([ -n "$1" ] && echo "-us" || echo "-u") $IMG_NAME
 
 if [[ $? == 0 ]]
 then
-    notify-send 'Snap!' 'Screenshot saved and copied to clipboard' | xclip -selection clipboard -t image/png 
+    xclip -selection clipboard -t image/png -i $IMG_NAME
+    notify-send 'Snap!' "Screenshot saved and copied to clipboard.\n($IMG_NAME)"
 fi
+    
